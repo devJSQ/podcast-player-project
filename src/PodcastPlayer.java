421abf6c5a853662c87c781c85ff6a47597c226a
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 04:42:32 +0300

    Update PodcastPlayer.java

--- src/PodcastPlayer.java
+++ src/PodcastPlayer.java
@@ -1,6 +1,25 @@
+
+
 public class PodcastPlayer {
     public static void main(String[] args) {
-      /* add your code here */
-    }
+        PodcastStack playbackHistory = new PodcastStack();
 
+        // Add 10 podcasts
+        for (int i = 1; i <= 10; i++) {
+            playbackHistory.add(new Podcast(
+                "Episode " + i,
+                "Channel " + i,
+                "Speaker " + i,
+                "Guest " + i,
+                30 + i
+            ));
+        }
+
+        playbackHistory.printAll();
+        System.out.println("\n> Playing most recent podcast:");
+        playbackHistory.play();
+        System.out.println("\n> Going to previous podcast:");
+        playbackHistory.previous();
+        playbackHistory.printAll();
+    }
 }
